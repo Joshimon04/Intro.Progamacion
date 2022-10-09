8b4d9de61e6e51c6e1b4b4a20585b7650e671496
Joshimon04 <111013098+Joshimon04@users.noreply.github.com>
2022-10-08 22:46:14 -0600

    Add files via upload

---
+++ L9+_+DJRR+1026122_/L9+_+DJRR+1026122_/Automovil.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L9___DJRR_1026122_
+{
+    private static void Main(string[] args)
+        {
+            Automovil objAutomovil = new Automovil(2019, 10000.00, "", false, 7.50, 0.00);
+
+            Console.WriteLine("Ingrese la marca del automóvil:");
+            objAutomovil.DefinirMarca(Console.ReadLine());
+
+            // Se siguen pidiendo los datos...
+
+            // Aquí mostramos toda la info:
+            Console.WriteLine(objAutomovil.MostrarInformacion());
+
+            // Esto es para que la consola no se cierre:
+            Console.ReadKey();
+        }
+
+
+    class Automovil
+    {
+        // Atributos privados:
+        private int modelo;
+        private double precio;
+        private string marca;
+        private bool disponible;
+        private double tipoCambioDolar;
+        private double descuentoAplicado;
+
+        // Métodos públicos:
+        // Este public Automovil es el constructor
+        public Automovil(int Modelo, double Precio, string Marca, bool Disponible, double TipoCambioDolar, double DescuentoAplicado)
+        {
+            modelo = Modelo;
+            precio = Precio;
+            marca = Marca;
+            disponible = Disponible;
+            tipoCambioDolar = TipoCambioDolar;
+            descuentoAplicado = DescuentoAplicado;
+        }
+
+        public void DefinirModelo(int unModelo)
+        {
+            modelo = unModelo;
+        }
+
+        public void DefinirPrecio(double unPrecio)
+        {
+            precio = unPrecio;
+        }
+
+        public void DefinirMarca(string unaMarca)
+        {
+            marca = unaMarca;
+        }
+
+        public void DefinirTipoCambio(double unTipoCambio)
+        {
+            tipoCambioDolar = unTipoCambio;
+        }
+
+        public void CambiarDisponibilidad()
+        {
+            if (disponible == true)
+            {
+                disponible = false;
+            }
+            else
+            {
+                disponible = true;
+            }
+        }
+
+        public string MostrarDisponibilidad()
+        {
+            if (disponible == true)
+            {
+                return "Disponible";
+            }
+            else
+            {
+                return "No se encuentra disponible actualmente";
+            }
+        }
+
+        // Aquí se muestran todas las variables:
+        public string MostrarInformacion()
+        {
+            return "Marca: " + marca + ", precio: " + precio;
+        }
+    }
+
+}
